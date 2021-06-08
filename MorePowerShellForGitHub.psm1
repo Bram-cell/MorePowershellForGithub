3db2b6965044cdf523d89ae51c63ce4be5953082
Bram <Bram.Bellic@hotmail.com>
2021-06-08 22:05:42 +0200

    Adding a Help Function                 This is now a module with a help function (synopsis) added to it. Allows the user to ask for help and it will give the asked parameters and give an example.

--- MorePowerShellForGitHub.psm1
+++ MorePowerShellForGitHub.psm1
@@ -1,3 +1,4 @@
+
 function Get-AuthHeader  {
     param (
         [System.Management.Automation.PSCredential]$Credentials
@@ -6,4 +7,29 @@ function Get-AuthHeader  {
     $auth = [System.Convert]::ToBase64String([char[]]$Creds.GetNetworkCredential().Password)
     $headers = @{Authorization="Basic $auth"}
     Invoke-RestMethod -Headers $headers https://api.github.com
+    <#
+    .Synopsis 
+    Dit is een functie die er voor zorgt dat u automatisch kan aanloggen op github.
+    -Credentials: een password/username in PSCredentials 
+    .Example Get-AuthHeader -Credentials $C
+    #>
+}
+
+function Add-githubCollaborator {
+    param (
+        [System.Management.Automation.PSCredential]$Credentials,
+        [string]$Collaborator,
+        [string]$Repository
+    )
+    Get-AuthHeader($Credentials)
+    Get-GitHubRepository($Repository)
+    <#
+    .Synopsis 
+    Dit is een functie die er voor zorgt dat u automatisch een collaborator kan toevoegen op github.
+    -Credentials: een password/username in PSCredentials 
+    -Collaborator: de persoon die u wilt toevoegen als collaborator
+    -Repository: De repository aan wat u de Collaborator wilt toevoegen.
+    .Example Get-AuthHeader -Credentials $C -Collaborator Bram-cell -Repository Test
+    #>
+}
 }
